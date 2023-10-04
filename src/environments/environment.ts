d553167fb76afdcf97be6f799bbd5e7da01f77ff
Dojelin Quijada <dojelin@gmail.com>
2023-10-04 14:57:13 -0400

    add firebase and material

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCb_nIPwDxFSH3Sl7N3HdRqU0NOtquI9BU',
+    authDomain: 'ng-fire-6f85c.firebaseapp.com',
+    projectId: 'ng-fire-6f85c',
+    storageBucket: 'ng-fire-6f85c.appspot.com',
+    messagingSenderId: '973667857444',
+    appId: '1:973667857444:web:b5507b81495b7f54c314b1',
+    measurementId: 'G-WJNX88H62V',
+  },
+};
